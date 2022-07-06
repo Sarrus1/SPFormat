aecdf428b6247eee9b1b39512a26d293d62b8c9b
Sarrus1 <omegasix0611@gmail.com>
2022-07-06 12:13:36 +0200

    Chore: code rework

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::{fs, str::Utf8Error};
 use tree_sitter::{Node, Parser};
 
+struct Formatter<'a> {
+    output: String,
+    source: &'a [u8],
+}
+
 fn main() -> Result<(), Utf8Error> {
     let mut parser = Parser::new();
     parser
@@ -9,24 +14,24 @@ fn main() -> Result<(), Utf8Error> {
     let filename = "test.sp";
     let source =
         fs::read_to_string(filename).expect("Something went wrong while reading the file.");
-    let source = source.as_bytes();
 
     let parsed = parser.parse(&source, None).unwrap();
     let mut cursor = parsed.walk();
-    let mut output = "".to_string();
+    let mut formatter = Formatter {
+        output: String::new(),
+        source: source.as_bytes(),
+    };
     for node in parsed.root_node().children(&mut cursor) {
-        let mut buf = String::new();
         match node.kind() {
-            "global_variable_declaration" => write_global_variable(node, &mut buf, &source)?,
-            _ => buf = node.utf8_text(source)?.to_string(),
+            "global_variable_declaration" => write_global_variable(node, &mut formatter)?,
+            _ => formatter.output.push_str(node.utf8_text(formatter.source)?),
         };
-        output.push_str(buf.as_str());
     }
-    fs::write(filename, output).expect("Something went wrong writing the file.");
+    fs::write(filename, formatter.output).expect("Something went wrong writing the file.");
     Ok(())
 }
 
-fn write_global_variable(node: Node, buf: &mut String, source: &[u8]) -> Result<(), Utf8Error> {
+fn write_global_variable(node: Node, formatter: &mut Formatter) -> Result<(), Utf8Error> {
     let mut cursor = node.walk();
     let mut variable_declarations: Vec<Node> = Vec::new();
 
@@ -34,8 +39,10 @@ fn write_global_variable(node: Node, buf: &mut String, source: &[u8]) -> Result<
     for sub_node in node.named_children(&mut cursor) {
         match sub_node.kind() {
             "variable_storage_class" | "variable_visibility" | "type" => {
-                buf.push_str(sub_node.utf8_text(source)?);
-                buf.push(' ');
+                formatter
+                    .output
+                    .push_str(sub_node.utf8_text(formatter.source)?);
+                formatter.output.push(' ');
             }
             "variable_declaration" => variable_declarations.push(sub_node),
             _ => println!("{}", sub_node.kind()),
@@ -53,32 +60,36 @@ fn write_global_variable(node: Node, buf: &mut String, source: &[u8]) -> Result<
         let var_name = child
             .child_by_field_name("name")
             .unwrap()
-            .utf8_text(source)?;
-        buf.push_str(var_name);
+            .utf8_text(formatter.source)?;
+        formatter.output.push_str(var_name);
 
         let mut cursor = child.walk();
         // Write the dimensions of a declaration, if they exist.
         for sub_child in child.named_children(&mut cursor) {
             if sub_child.kind() == "fixed_dimension" || sub_child.kind() == "dimension" {
-                buf.push_str(sub_child.utf8_text(source)?);
+                formatter
+                    .output
+                    .push_str(sub_child.utf8_text(formatter.source)?);
             }
         }
 
         // Write the default value of a declaration, if it exists.
         for sub_child in child.children_by_field_name("initialValue", &mut cursor) {
             if sub_child.kind() == "=" {
-                buf.push_str(" = ");
+                formatter.output.push_str(" = ");
                 continue;
             }
-            buf.push_str(sub_child.utf8_text(source)?);
+            formatter
+                .output
+                .push_str(sub_child.utf8_text(formatter.source)?);
             break;
         }
-        buf.push_str(", ");
+        formatter.output.push_str(", ");
     }
 
     // Remove the last ", "
-    buf.pop();
-    buf.pop();
-    buf.push_str(";\n");
+    formatter.output.pop();
+    formatter.output.pop();
+    formatter.output.push_str(";\n");
     Ok(())
 }

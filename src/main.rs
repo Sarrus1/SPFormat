21f1c2467016cb5c718d188e75c57cb343029436
Sarrus1 <omegasix0611@gmail.com>
2022-07-06 11:56:36 +0200

    Chore: code rework

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
-use std::fs;
+use std::{fs, str::Utf8Error};
 use tree_sitter::{Node, Parser};
 
-fn main() {
+fn main() -> Result<(), Utf8Error> {
     let mut parser = Parser::new();
     parser
         .set_language(tree_sitter_sourcepawn::language())
         .expect("Error loading SourcePawn grammar");
     let filename = "test.sp";
-    let source = fs::read_to_string(filename).expect("Something went wrong reading the file.");
+    let source =
+        fs::read_to_string(filename).expect("Something went wrong while reading the file.");
+    let source = source.as_bytes();
+
     let parsed = parser.parse(&source, None).unwrap();
     let mut cursor = parsed.walk();
     let mut output = "".to_string();
     for node in parsed.root_node().children(&mut cursor) {
         let mut buf = String::new();
         match node.kind() {
-            "global_variable_declaration" => write_global_variable(node, &mut buf, &source),
-            _ => buf = node.utf8_text(source.as_ref()).unwrap().to_string(),
+            "global_variable_declaration" => write_global_variable(node, &mut buf, &source)?,
+            _ => buf = node.utf8_text(source)?.to_string(),
         };
         output.push_str(buf.as_str());
     }
     fs::write(filename, output).expect("Something went wrong writing the file.");
+    Ok(())
 }
 
-fn write_global_variable(node: Node, buf: &mut String, source: &String) {
+fn write_global_variable(node: Node, buf: &mut String, source: &[u8]) -> Result<(), Utf8Error> {
     let mut cursor = node.walk();
     let mut variable_declarations: Vec<Node> = Vec::new();
 
@@ -30,7 +34,7 @@ fn write_global_variable(node: Node, buf: &mut String, source: &String) {
     for sub_node in node.named_children(&mut cursor) {
         match sub_node.kind() {
             "variable_storage_class" | "variable_visibility" | "type" => {
-                buf.push_str(sub_node.utf8_text(source.as_ref()).unwrap());
+                buf.push_str(sub_node.utf8_text(source)?);
                 buf.push(' ');
             }
             "variable_declaration" => variable_declarations.push(sub_node),
@@ -49,15 +53,14 @@ fn write_global_variable(node: Node, buf: &mut String, source: &String) {
         let var_name = child
             .child_by_field_name("name")
             .unwrap()
-            .utf8_text(source.as_ref())
-            .unwrap();
+            .utf8_text(source)?;
         buf.push_str(var_name);
 
         let mut cursor = child.walk();
         // Write the dimensions of a declaration, if they exist.
         for sub_child in child.named_children(&mut cursor) {
             if sub_child.kind() == "fixed_dimension" || sub_child.kind() == "dimension" {
-                buf.push_str(sub_child.utf8_text(source.as_ref()).unwrap());
+                buf.push_str(sub_child.utf8_text(source)?);
             }
         }
 
@@ -67,7 +70,7 @@ fn write_global_variable(node: Node, buf: &mut String, source: &String) {
                 buf.push_str(" = ");
                 continue;
             }
-            buf.push_str(sub_child.utf8_text(source.as_ref()).unwrap());
+            buf.push_str(sub_child.utf8_text(source)?);
             break;
         }
         buf.push_str(", ");
@@ -76,6 +79,6 @@ fn write_global_variable(node: Node, buf: &mut String, source: &String) {
     // Remove the last ", "
     buf.pop();
     buf.pop();
-    buf.push(';');
-    buf.push('\n');
+    buf.push_str(";\n");
+    Ok(())
 }
--- test.sp
+++ test.sp
@@ -1,4 +1,4 @@
 #include <sourcemod>
 
-int foo = 1, bar;
+int foo = 1, bar = 10;
 int baz[MAXPLAYERS+1];

a09a4233ee8153060a79ece95c0fe49308ffc8a5
Sarrus1 <omegasix0611@gmail.com>
2022-07-07 00:59:21 +0200

    Feat: revert changes

--- tree-sitter-sourcepawn/bindings/rust/build.rs
+++ tree-sitter-sourcepawn/bindings/rust/build.rs
@@ -3,7 +3,6 @@ fn main() {
 
     let mut c_config = cc::Build::new();
     c_config.include(&src_dir);
-    c_config.include(std::path::Path::new("/usr/include"));
     c_config
         .flag_if_supported("-Wno-unused-parameter")
         .flag_if_supported("-Wno-unused-but-set-variable")

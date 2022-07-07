48706d2f6337cfe4d518e87f40cbdc602fe9b275
Sarrus1 <omegasix0611@gmail.com>
2022-07-07 17:27:57 +0200

    Update workflow

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/SPFormat/",
   server: {
     fs: {
       // Allow serving files from one level up to the project root

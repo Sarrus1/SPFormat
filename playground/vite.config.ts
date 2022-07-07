1dbdccda33ab46594d2cf3725e5d625f50bcfea5
Sarrus1 <omegasix0611@gmail.com>
2022-07-07 17:16:16 +0200

    Feat: new build

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -16,4 +16,19 @@ export default defineConfig({
       filter: /.+\.wasm$/,
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".")[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
+  },
 });

339ec5fde08382117b596b2ba278a1525b3cfc0d
Sarrus1 <omegasix0611@gmail.com>
2022-08-09 15:28:57 +0200

    Feat: update playground

--- playground/src/components/header.tsx
+++ playground/src/components/header.tsx
@@ -1,8 +1,24 @@
-import { Box, Toolbar, Button, AppBar, Typography } from "@mui/material";
+import {
+  Box,
+  Toolbar,
+  Button,
+  AppBar,
+  Typography,
+  Snackbar,
+} from "@mui/material";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import packageJson from "../../package.json";
+import { useState, forwardRef } from "react";
 
 import { sp_format } from "sp_format";
 import { Settings } from "../interfaces";
+
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 interface HeaderProps {
   readonly code: string;
@@ -21,20 +37,56 @@ export default function Header(props: HeaderProps) {
           <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
             SPFormat v{packageJson.version}
           </Typography>
-          <Button
-            color="primary"
-            variant="contained"
-            style={{ backgroundColor: "grey", marginLeft: "auto" }}
-            onClick={(e) => {
-              sp_format(props.code, props.settings as Settings)
-                .then((res) => props.setCode(res))
-                .catch((err) => console.log(err));
-            }}
-          >
-            Format
-          </Button>
+          <FormatButton {...props} />
         </Toolbar>
       </AppBar>
     </Box>
+  );
+}
+
+function FormatButton(props: HeaderProps) {
+  const [showError, setShowError] = useState(false);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setShowError(false);
+  };
+
+  return (
+    <>
+      <Button
+        color="primary"
+        variant="contained"
+        style={{ backgroundColor: "grey", marginLeft: "auto" }}
+        onClick={(e) => {
+          sp_format(props.code, props.settings as Settings)
+            .then((res) => {
+              if (res.length == 0 && props.code.trim().length > 0) {
+                setShowError(true);
+              } else {
+                props.setCode(res);
+              }
+            })
+            .catch((err) => console.log(err));
+        }}
+      >
+        Format
+      </Button>
+      <Snackbar
+        open={showError}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          There is an error in your syntax.
+        </Alert>
+      </Snackbar>
+    </>
   );
 }

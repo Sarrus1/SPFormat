559d2befcbda19b9c78788c8697ab05bf9dcde88
Charles <63302440+Sarrus1@users.noreply.github.com>
2024-04-11 11:45:31 -0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let src_path = std::path::PathBuf::from("vendor/llvm-project/");
-    let clang_include = src_path.join("clang/include/");
-    let llvm_include = src_path.join("llvm/include/");
-    let llvm_include_overlay = src_path.join("utils/bazel/llvm-project-overlay/llvm/include/");
+    let src_path = std::path::PathBuf::from("vendor/llvm-project/llvm");
+    let clang_include = src_path.join("include/");
+    let llvm_include = src_path.join("../llvm/include/");
+    let llvm_include_overlay = src_path.join("../utils/bazel/llvm-project-overlay/llvm/include/");
     // vendor\llvm-project\utils\bazel\llvm-project-overlay\llvm\include
     // let mut b = autocxx_build::Builder::new(
     //     "src/main.rs",
@@ -22,12 +22,14 @@ fn main() {
     //     .compile("autocxx-demo"); // arbitrary library name, pick anything
     // println!("cargo:rerun-if-changed=src/main.rs");
     // Add instructions to link to any C++ libraries you need.
-    let dst = cmake::Config::new(src_path)
-        .build_arg("-S vendor/llvm-project/llvm -B vendor/llvm-project/build")
-        .build_arg("-DBUILD_SHARED_LIBS=OFF")
-        .build_arg("-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\"")
-        // .define("FOO", "BAR")
-        .build();
+    let dst =
+        cmake::Config::new("/Users/charleszablit/Public/foo/SPFormat/vendor/llvm-project/llvm")
+            // .configure_arg("-S /Users/charleszablit/Public/foo/SPFormat/vendor/llvm-project/llvm")
+            // .configure_arg("-B /Users/charleszablit/Public/foo/SPFormat/vendor/llvm-project/build")
+            .configure_arg("-DBUILD_SHARED_LIBS=OFF")
+            .configure_arg("-DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra")
+            .build_target("clang-format")
+            .build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=foo");
 }
